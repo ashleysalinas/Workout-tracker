0917c7667559965744d6f6bf7cad30cd2a1c7d76
ashleysalinas <ashleysalinas98@protonmail.com>
2022-01-26 12:30:46 -0600

    fixed .env bug

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 const db = require('../models/workout');
-require('dotenv').config();
-
-mongoose.connect("mongodb+srv://ashleysalinas:root@Cluster0.ja3k3.mongodb.net/Cluster0?retryWrites=true&w=majority", {
+require("dotenv").config({path:'../.env'});
+ mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
