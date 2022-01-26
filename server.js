3562b22f05185572b736f48d9241b034058ed6ec
ashleysalinas <ashleysalinas98@protonmail.com>
2022-01-26 11:41:46 -0600

    added server & mongodb connect

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config()
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+})
